d24613a63ae0de3a96048c6742c28ded3bcd28cf
Justin Yost <justin.yost@yostivanich.com>
2012-10-04 01:06:05 -0500

    Start of Post Receive Hook
    Signed-off-by: Justin Yost <justin.yost@yostivanich.com>

---
+++ post-recieve-hook.php
@@ -0,0 +1,13 @@
+<?php
+//This is not a valid GitHub Post Recieve Hook, ignore
+if (!array_key_exists('payload', $_POST)) {
+	exit;
+}
+
+//Extract GitHub Payload
+extract(json_decode(stripslashes($_POST['payload']), true));
+
+//Basic Pull Rebase
+exec('git pull --rebase');
+
+?>
